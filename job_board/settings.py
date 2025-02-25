8069759820e6a0b27a13b031cbcdaafd7c3bb0bc
unknown <SanuMarma.sanumarma1717@gmail.comgit config --global user.email SanuMarma.sanumarma1717@gmail.com>
2025-02-25 21:06:05 +0600

    cros origin resolved

--- job_board/settings.py
+++ job_board/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = 'django-insecure-tm9^s223bw3ajz+7)jwod9oeu23o815ugr5(gs_-dv14a)ud4-
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = ['https://job-board-bc02.onrender.com', 'https://*.127.0.0.1']
 
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'application',
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'job_board.urls'
